e455175b0c105b0cfa00d4b87c1cde69a6f4a1c1
chermnolesye <119154985+chermnolesye@users.noreply.github.com>
2025-04-16 20:05:12 +0300

    Добавлены формы аннотаций, изменена модель Text, все надо проверять

--- text_app/forms.py
+++ text_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from core_app.models import User, Student, Group, Rights
 from datetime import date
-from core_app.models import Text, TextType, WritePlace, WriteTool, Emotion, Group, Student
+from core_app.models import Text, TextType, WritePlace, WriteTool, Emotion, Group, Student, Error, ErrorTag, ErrorLevel, Reason
 
 class TeacherLoadTextForm(forms.ModelForm):
     group = forms.ModelChoiceField(
@@ -76,5 +76,99 @@ class TeacherLoadTextForm(forms.ModelForm):
         widget=forms.NumberInput(attrs={'min': 1, 'max': 10, 'step': 1}) 
     )
 
-class AddTextAnnotationForm(forms.Form):
-    text_grade = 1
+
+class AddTextAnnotationForm(forms.ModelForm):
+    textgrade = forms.ChoiceField(
+        choices=Text.TASK_RATES,
+        label="Оценка",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    completeness = forms.ChoiceField(
+        choices=Text.TASK_RATES,
+        label="Полнота раскрытия",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    structure = forms.ChoiceField(
+        choices=Text.TASK_RATES,
+        label="Структура",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    coherence = forms.ChoiceField(
+        choices=Text.TASK_RATES,
+        label="Связность",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    poscheckflag = forms.TypedChoiceField(
+        choices=[(None, "Не указано"), (True, "Проверено")],
+        coerce=lambda x: x == 'True',
+        required=False,
+        label="Проверка частеречной разметки"
+    )
+
+    errorcheckflag = forms.TypedChoiceField(
+        choices=[(None, "Не указано"), (True, "Проверено")],
+        coerce=lambda x: x == 'True',
+        required=False,
+        label="Проверка разметки ошибок"
+    )
+
+    class Meta:
+        model = Text
+        fields = [
+            'textgrade',
+            'completeness',
+            'structure',
+            'coherence',
+            'poscheckflag',
+            'errorcheckflag'
+        ]
+
+
+class AddErrorAnnotationForm(forms.ModelForm):
+    iderrortag = forms.ModelChoiceField(
+        queryset=ErrorTag.objects.all(),
+        label="Выберите тег",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    idreason = forms.ModelChoiceField(
+        queryset=Reason.objects.all(),
+        label="Причина ошибки",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    iderrorlevel = forms.ModelChoiceField(
+        queryset=ErrorLevel.objects.all(),
+        label="Степень грубости",
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
+    comment = forms.CharField(
+        label="Комментарий",
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+        required=False
+    )
+
+    class Meta:
+        model = Error
+        fields = ['iderrortag', 'idreason', 'iderrorlevel', 'comment']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+    def get_display_info(self):
+        selected_tag = self.cleaned_data.get('iderrortag')
+        creator_name = (
+            f"{self.user.firstname} {self.user.lastname}"
+            if self.user and hasattr(self.user, 'firstname') and hasattr(self.user, 'lastname')
+            else "Неизвестный пользователь"
+        )
+        return {
+            'selected_tag': selected_tag.tagtext if selected_tag else '',
+            'creator': creator_name
+        }
