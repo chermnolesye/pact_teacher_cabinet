647196b0758732426129326023796abe199997e1
chermnolesye <119154985+chermnolesye@users.noreply.github.com>
2025-04-21 23:18:34 +0300

    AddGroupForm

--- years_and_groups_app/forms.py
+++ years_and_groups_app/forms.py
@@ -3,93 +3,114 @@ from core_app.models import Group, AcademicYear, Student
 import datetime
 from django.forms import formset_factory
 
-class AddGroupForm(forms.Form):
+class AddGroupForm(forms.ModelForm):
     class Meta:
         model = Group
-        fields = ['groupname', 'title', 'studycourse']
-        
-    groupname = forms.CharField(label="Название группы", required=True)
-    studycourse = forms.IntegerField(label="Курс обучения", required = True, widget=forms.NumberInput(attrs={'id': 'studycourse'}), min_value=1, max_value=5)
-    current_year = datetime.datetime.now().year
-    title = forms.IntegerField(label="Учебный год", required=True, min_value=current_year, max_value=current_year+1, initial=current_year, widget=forms.NumberInput(attrs={'id': 'title'}))
-    title_2 = forms.CharField(initial=f"/ {current_year+1}", label="Учебный год конечный", widget=forms.TextInput(attrs={'id': 'title_2', 'readonly': 'readonly'})) # второе поле для конечного года    
-    idayear = forms.ModelChoiceField(label="Id года", required = False, widget=forms.HiddenInput(), queryset=AcademicYear.objects.all()) # для id FK
-    
+        fields = ['groupname', 'studycourse', 'idayear']
+        labels = {
+            'groupname': 'Название группы',
+            'studycourse': 'Курс',
+            'idayear': 'Учебный год',
+        }
+        widgets = {
+            'groupname': forms.TextInput(attrs={'class': 'form-control'}),
+            'studycourse': forms.Select(choices=[(i, str(i)) for i in range(1, 6)],
+                                        attrs={'class': 'form-select'}),
+            'idayear': forms.Select(attrs={'class': 'form-select'}),
+        }
+
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        ###########
-        self.available_students = self.get_available_students()
-        self.initial_student_field_count = 1
+        super(AddGroupForm, self).__init__(*args, **kwargs)
+        self.fields['idayear'].queryset = AcademicYear.objects.all().order_by('-title')
+
+
+# class AddGroupForm(forms.Form):
+#     class Meta:
+#         model = Group
+#         fields = ['groupname', 'title', 'studycourse']
         
-        for i in range(1, self.initial_student_field_count + 1):
-            self.fields[f'student_{i}'] = self.get_student_field(i)
+#     groupname = forms.CharField(label="Название группы", required=True)
+#     studycourse = forms.IntegerField(label="Курс обучения", required = True, widget=forms.NumberInput(attrs={'id': 'studycourse'}), min_value=1, max_value=5)
+#     current_year = datetime.datetime.now().year
+#     title = forms.IntegerField(label="Учебный год", required=True, min_value=current_year, max_value=current_year+1, initial=current_year, widget=forms.NumberInput(attrs={'id': 'title'}))
+#     title_2 = forms.CharField(initial=f"/ {current_year+1}", label="Учебный год конечный", widget=forms.TextInput(attrs={'id': 'title_2', 'readonly': 'readonly'})) # второе поле для конечного года    
+#     idayear = forms.ModelChoiceField(label="Id года", required = False, widget=forms.HiddenInput(), queryset=AcademicYear.objects.all()) # для id FK
+    
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         ###########
+#         self.available_students = self.get_available_students()
+#         self.initial_student_field_count = 1
+        
+#         for i in range(1, self.initial_student_field_count + 1):
+#             self.fields[f'student_{i}'] = self.get_student_field(i)
         
     
-    def get_available_students(self):
-        students = Student.objects.filter(idgroup=81).select_related('iduser') #надо изменить id группы на что-то
-        return [(s.idstudent, f"{s.iduser.firstname} {s.iduser.lastname}  {s.iduser.middlename}") for s in students]
+#     def get_available_students(self):
+#         students = Student.objects.filter(idgroup=81).select_related('iduser') #надо изменить id группы на что-то
+#         return [(s.idstudent, f"{s.iduser.firstname} {s.iduser.lastname}  {s.iduser.middlename}") for s in students]
 
-    def get_student_field(self, index):
-        return forms.ChoiceField(
-            choices=[('', 'Выберите студента')] + self.available_students,
-            required=False,
-            label=f"Студент {index}"
-        )
-        ############
-    def clean(self):
-        cleaned_data = super().clean()
-        groupname = cleaned_data.get('groupname')
-        title = cleaned_data.get('title')
-        studycourse = cleaned_data.get('studycourse')
+#     def get_student_field(self, index):
+#         return forms.ChoiceField(
+#             choices=[('', 'Выберите студента')] + self.available_students,
+#             required=False,
+#             label=f"Студент {index}"
+#         )
+#         ############
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         groupname = cleaned_data.get('groupname')
+#         title = cleaned_data.get('title')
+#         studycourse = cleaned_data.get('studycourse')
 
-        if groupname and title and studycourse:
-            current_year = datetime.datetime.now().year
-            if title < current_year:
-                raise forms.ValidationError("Группу можно создавать только для текущего учебного года или последующих годов.")
-            try:
-                academic_year = AcademicYear.objects.get(title__startswith=str(title))
+#         if groupname and title and studycourse:
+#             current_year = datetime.datetime.now().year
+#             if title < current_year:
+#                 raise forms.ValidationError("Группу можно создавать только для текущего учебного года или последующих годов.")
+#             try:
+#                 academic_year = AcademicYear.objects.get(title__startswith=str(title))
 
-            except AcademicYear.DoesNotExist:
-                academic_year = AcademicYear.objects.create(title=f"{title}/{title+1}")
-            cleaned_data['idayear'] = academic_year
-            ##########
-            selected_students = []
-            for field_name in self.fields:
-                if field_name.startswith('student_'):
-                    student = cleaned_data.get(field_name)
-                    if student:
-                        selected_students.append(student)
+#             except AcademicYear.DoesNotExist:
+#                 academic_year = AcademicYear.objects.create(title=f"{title}/{title+1}")
+#             cleaned_data['idayear'] = academic_year
+#             ##########
+#             selected_students = []
+#             for field_name in self.fields:
+#                 if field_name.startswith('student_'):
+#                     student = cleaned_data.get(field_name)
+#                     if student:
+#                         selected_students.append(student)
 
-            if len(selected_students) != len(set(selected_students)):
-                raise forms.ValidationError("Вы не можете выбрать одного и того же студента несколько раз.")
-            ############
-        return cleaned_data
+#             if len(selected_students) != len(set(selected_students)):
+#                 raise forms.ValidationError("Вы не можете выбрать одного и того же студента несколько раз.")
+#             ############
+#         return cleaned_data
     
-    def save(self):
-        if self.is_valid():
-            groupname = self.cleaned_data['groupname']
-            studycourse = self.cleaned_data['studycourse']
-            idayear = self.cleaned_data['idayear']
-            student_id = self.cleaned_data.get('student')
-            try:
-                Group.objects.get(groupname=groupname, studycourse=studycourse, idayear=idayear)
-            except Group.DoesNotExist:
-                group = group = Group.objects.create(groupname=groupname, studycourse=studycourse, idayear=idayear)
-                ###########
-                for field_name in self.fields:
-                    if field_name.startswith('student_'):
-                        student = self.cleaned_data.get(field_name)
-                        #student = form.cleaned_data.get('student')
-                        if student:
-                            try:
-                                student = Student.objects.get(pk=student) #надо изменить id группы на что-то
-                                student.idgroup = group
-                                student.save()
-                            except:
-                                raise forms.ValidationError("Такого судента нет")
-                #########
-                return group
-        return None
+#     def save(self):
+#         if self.is_valid():
+#             groupname = self.cleaned_data['groupname']
+#             studycourse = self.cleaned_data['studycourse']
+#             idayear = self.cleaned_data['idayear']
+#             student_id = self.cleaned_data.get('student')
+#             try:
+#                 Group.objects.get(groupname=groupname, studycourse=studycourse, idayear=idayear)
+#             except Group.DoesNotExist:
+#                 group = group = Group.objects.create(groupname=groupname, studycourse=studycourse, idayear=idayear)
+#                 ###########
+#                 for field_name in self.fields:
+#                     if field_name.startswith('student_'):
+#                         student = self.cleaned_data.get(field_name)
+#                         #student = form.cleaned_data.get('student')
+#                         if student:
+#                             try:
+#                                 student = Student.objects.get(pk=student) #надо изменить id группы на что-то
+#                                 student.idgroup = group
+#                                 student.save()
+#                             except:
+#                                 raise forms.ValidationError("Такого судента нет")
+#                 #########
+#                 return group
+#         return None
 
 
 
