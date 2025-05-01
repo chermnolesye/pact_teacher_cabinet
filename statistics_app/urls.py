43472540be924bb5518960f0fb85b845a91d3b2e
chermnolesye <119154985+chermnolesye@users.noreply.github.com>
2025-05-01 23:07:09 +0300

    -

--- statistics_app/urls.py
+++ statistics_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.statistics_view, name='statistics'), 
     path('error_stats', views.error_stats, name='error_stats'), 
     path('dashboard_error_types', views.chart_types_errors, name='dashboard_error_types'),
+    path("export_group_error_stats/", views.export_group_error_stats, name="export_group_error_stats"),
 ]
