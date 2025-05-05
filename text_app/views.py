e726077d22ec70ebf1a6123ca080f5de626e173e
Kate <150055439+mainClown@users.noreply.github.com>
2025-05-05 21:44:50 +0300

    trash

--- text_app/views.py
+++ text_app/views.py
@@ -171,7 +171,7 @@ def annotate_text(request, text_id=2379):
             pos_tag_color = token.idpostag.tagcolor if token.idpostag else None
 
             error_tokens = token.errortoken_set.select_related(
-                "iderror__iderrortag", "iderror__iderrorlevel", "iderror__idreason"
+                "iderror__iderrortag", "iderror__iderrorlevel", "iderror__idreason", "iderror"
             ).all()
 
             errors_list = []
@@ -179,6 +179,7 @@ def annotate_text(request, text_id=2379):
                 error = et.iderror
                 if error and error.iderrortag:
                     errors_list.append({
+                        "error_id": error.iderror,
                         "error_tag": error.iderrortag.tagtext,
                         "error_tag_russian": error.iderrortag.tagtextrussian,
                         "error_tag_abbrev": error.iderrortag.tagtextabbrev,
@@ -215,9 +216,10 @@ def annotate_text(request, text_id=2379):
     else:
         grade_form = AddTextAnnotationForm(instance=text)
     
-    if request.method == 'POST' and 'delete-annotation' in request.POST:
-            chosen_ids = json.loads(request.POST.get('chosen_ids', '[]'))
-            
+    if request.method == 'POST' and 'delete' in request.POST:
+            # chosen_ids = json.loads(request.POST.get('chosen_ids', '[]'))
+            error_id = json.loads(request.POST.get('error_id', ''))
+            print(error_id)
             error_tokens = ErrorToken.objects.filter(idtoken__in=chosen_ids)
         
             error_ids = error_tokens.values_list('iderror', flat=True).distinct()
